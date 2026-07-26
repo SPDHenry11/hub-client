4bb26e4894e0e5c64fc3d0a759d0c985e2952858
SPDHenry11 <esl-henrique-2010@hotmail.com>
2026-07-26 00:27:15 +0000

    minigames UI test

--- src/components/MinigameSelection.tsx
+++ src/components/MinigameSelection.tsx
@@ -1,17 +1,121 @@
-type Props = {
-	onLaunchGame: (
-		url: string
-	) => void;
+import "./MinigameSelection.css";
+
+export type Minigame = {
+	id: string;
+	title: string;
+	image: string;
+	url: string;
 };
 
-function MinigameSelection(
-	_props: Props
-) {
+type Props = {
+	onLaunchGame: (url: string) => void;
+};
+
+const minigames: Minigame[] = [
+	{
+		id: "cups-and-balls",
+		title: "Cups & Balls",
+		image: "/minigames/cups-and-balls.png",
+		url: "https://example.com/cups-and-balls",
+	},
+		{
+		id: "cups-and-balls",
+		title: "Cups & Balls",
+		image: "/minigames/cups-and-balls.png",
+		url: "https://example.com/cups-and-balls",
+	},
+		{
+		id: "cups-and-balls",
+		title: "Cups & Balls",
+		image: "/minigames/cups-and-balls.png",
+		url: "https://example.com/cups-and-balls",
+	},
+		{
+		id: "cups-and-balls",
+		title: "Cups & Balls",
+		image: "/minigames/cups-and-balls.png",
+		url: "https://example.com/cups-and-balls",
+	},
+		{
+		id: "cups-and-balls",
+		title: "Cups & Balls",
+		image: "/minigames/cups-and-balls.png",
+		url: "https://example.com/cups-and-balls",
+	},
+		{
+		id: "cups-and-balls",
+		title: "Cups & Balls",
+		image: "/minigames/cups-and-balls.png",
+		url: "https://example.com/cups-and-balls",
+	},
+		{
+		id: "cups-and-balls",
+		title: "Cups & Balls",
+		image: "/minigames/cups-and-balls.png",
+		url: "https://example.com/cups-and-balls",
+	},
+];
+
+function MinigameSelection({
+	onLaunchGame,
+}: Props) {
 
 	return (
-		<div>
+		<section
+			className="minigame-selection"
+		>
 
-		</div>
+			<div
+				className="minigame-grid"
+			>
+
+				{
+					minigames.map(
+						minigame => (
+							<button
+								key={
+									minigame.id
+								}
+								className="
+									minigame-card
+								"
+								onClick={() =>
+									onLaunchGame(
+										minigame.url
+									)
+								}
+							>
+
+								<img
+									className="
+										minigame-card-image
+									"
+									src={
+										minigame.image
+									}
+									alt={
+										minigame.title
+									}
+								/>
+
+								<div
+									className="
+										minigame-card-title
+									"
+								>
+									{
+										minigame.title
+									}
+								</div>
+
+							</button>
+						)
+					)
+				}
+
+			</div>
+
+		</section>
 	);
 }
 
